44bd1f87ec2a7ab4427d11204213e40a76e53766
Ravi <ravishankarsv@gmail.com>
2015-07-24 00:46:12 +0530

    CodeDeployTest

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>My first Blog</title>
+    </head>
+    <body>
+        <?php
+            echo "<p>Hello World!</p>
+    	    echo "<p>Version 1.0</p>
+            echo ' 
+            <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+            <html>
+            <img src="cloudcomputing1.png" width="280" height="125" title="Home Page Image" alt="Home Page Image" />
+            </html>
+        <a href="login.php"> Click here to login <br />
+        <a href="register.php"> Click here to register
+    ?>
+    </body>
+</html>
+
