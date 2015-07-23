f856bbf6095e4a2090c27dbc859c005c8e7b4886
Ravi <ravishankarsv@gmail.com>
2015-07-23 10:03:28 -0400

    Index Page

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>My first Blog</title>
+    </head>
+    <body>
+        <?php
+            echo "<p>Hello World!</p>
+            echo ' 
+            <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+            <html>
+            <img src="cloudcomputing1.png" width="280" height="125" title="Home Page Image" alt="Home Page Image" />
+            </html>
+        <a href="login.php"> Click here to login <br />
+        <a href="register.php"> Click here to register
+    ?>
+    </body>
+</html>
+
